bee9887b949766d9455169c9f62f7a004742bc72
Elissandro Silva <elissandro.soares@escolaweb.com.br>
2021-07-04 19:33:00 -0300

    added routes

---
+++ backend/src/router.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const route = express.Router()
+
+route.get('/', (req, res) => {
+    res.render('index')
+})
+
+route.get('/room', (req, res) => {
+    res.render('room')
+})
+
+route.get('/create-room', (req, res) => {
+    res.render('create-room')
+})
+
+route.post('/room/:room/:question/:action', (req, res) => {})
+
+module.exports = route
